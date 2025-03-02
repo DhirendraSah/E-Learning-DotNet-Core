395261079b4257485e7865ebd12ceaccb8dffe6a
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-02 23:25:50 +0530

    educator

---
+++ Models/EducatorDashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Learning.Models
+{
+	public class EducatorDashboardViewModel
+	{
+		[Key]
+		public int TotalEnrollments { get; set; }
+		public int TotalCourses { get; set; }
+		public decimal TotalEarnings { get; set; }
+		public List<Enrollment> RecentEnrollments { get; set; }
+	}
+}
