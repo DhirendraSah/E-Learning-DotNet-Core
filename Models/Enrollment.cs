a18c9c13b6f9a0c48878d294e2dba09ec324acc6
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-11 14:45:16 +0530

    enrollnment

---
+++ Models/Enrollment.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Learning.Models
+{
+	public class Enrollment
+	{
+		public int EnrollmentId { get; set; }
+		public int CourseId { get; set; }
+		public string StudentEmail { get; set; }
+
+		public Course Course { get; set; }
+	}
+
+}
