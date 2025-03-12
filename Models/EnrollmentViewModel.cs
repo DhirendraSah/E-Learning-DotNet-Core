edf25328eebd807ed514ed3de90ffdb2b314522a
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-12 08:59:56 +0530

    viewcourse

---
+++ Models/EnrollmentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Learning.Models
+{
+	public class EnrollmentViewModel
+	{
+		[Key]
+		public string StudentEmail { get; set; }
+
+		public string CourseTitle { get; set; }
+	}
+
+}
