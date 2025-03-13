b5a5278d5cb0bee5049c686931b71f18b7ed754b
Santosh <smahato575@rku.ac.in>
2025-03-13 22:16:49 +0530

    Roadmap control

---
+++ Controllers/RoadmapController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Learning.Controllers
+{
+	public class RoadmapController : Controller
+	{
+		[HttpGet("/Roadmap")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
