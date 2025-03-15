faf8deba18a0ce609b36a75f5e05df9d4e9feb39
Santosh <smahato575@rku.ac.in>
2025-03-15 22:26:59 +0530

    update in AppDbContext

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -2,21 +2,35 @@
 
 namespace E_Learning.Models
 {
-	public class AppDbContext : DbContext
-	{
-		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-		public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
-		public DbSet<Course> Courses { get; set; }
-		public DbSet<Lecture> Lectures { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
 
-		public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
 
-		public DbSet<LectureProgress> LectureProgresses { get; set; }
+        public DbSet<LectureProgress> LectureProgresses { get; set; }
 
-		public DbSet<EnrollmentViewModel> TotalEnrollments { get; set; }
+        public DbSet<EnrollmentViewModel> TotalEnrollments { get; set; }
 
-		public DbSet<EducatorDashboardViewModel> DashBoardData { get; set; }
-	}
+        public DbSet<EducatorDashboardViewModel> DashBoardData { get; set; }
+
+        public DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
+
+        public DbSet<CourseLike> CourseLikes { get; set; }
+
+        public DbSet<CourseFeedback> CourseFeedbacks { get; set; }
+
+        public DbSet<Quiz> Quizzes { get; set; }
+
+        public DbSet<QuizQuestion> QuizQuestions { get; set; }
+
+        public DbSet<Note> Notes { get; set; }
+
+
+    }
 }
