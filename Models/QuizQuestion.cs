c38ab56eb1f9c7ddab9bfdb40e4cc9656102d68c
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-18 22:45:21 +0530

    Quizesquestion

---
+++ Models/QuizQuestion.cs
@@ -0,0 +1,17 @@
+﻿namespace E_Learning.Models
+{
+	public class QuizQuestion
+	{
+		public int QuizQuestionId { get; set; }
+		public int QuizId { get; set; }
+		public string QuestionText { get; set; }
+		public string OptionA { get; set; }
+		public string OptionB { get; set; }
+		public string OptionC { get; set; }
+		public string OptionD { get; set; }
+		public string CorrectAnswer { get; set; }  // Store like "A", "B", "C", "D"
+
+		public Quiz Quiz { get; set; }
+	}
+
+}
