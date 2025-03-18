05954d9c4aaad314356f1c13dd4aeafe9fc1eea5
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-18 22:40:04 +0530

    Notes

---
+++ Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Learning.Models
+{
+	public class Note
+	{
+		public int NoteId { get; set; }
+
+		
+		public string Title { get; set; }
+
+		public string Content { get; set; }
+
+		
+		public string FilePath { get; set; }
+
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+	}
+}
