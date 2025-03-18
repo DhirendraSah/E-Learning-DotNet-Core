929e82c79c97941e12a248eebea1923095f43bc7
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-18 14:43:56 +0530

    progress

---
+++ Models/LectureProgress.cs
@@ -0,0 +1,12 @@
+﻿using E_Learning.Models;
+
+public class LectureProgress
+{
+	public int LectureProgressId { get; set; }
+	public int CourseId { get; set; }  // Link to the course
+	public int LectureId { get; set; } // Track specific lecture
+	public string StudentEmail { get; set; } // Track progress per student
+	public bool IsCompleted { get; set; } = false; // Completion status
+
+	public Lecture Lecture { get; set; }
+}
