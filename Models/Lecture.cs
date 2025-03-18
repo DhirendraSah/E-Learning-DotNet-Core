86232bb6f6439c9ad5140d24378d16261d19ff40
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-18 08:01:45 +0530

    lecture

---
+++ Models/Lecture.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Learning.Models
+{
+	public class Lecture
+	{
+		[Key]
+		public int LectureId { get; set; }
+
+		[Required]
+		public string LectureTitle { get; set; }
+
+		public string VideoPath { get; set; }
+
+		[ForeignKey("Course")]
+		public int CourseId { get; set; }
+		public Course Course { get; set; }
+	}
+}
