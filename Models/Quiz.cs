f7b881afd09d21df336db5b7abf55c3ec0fb40af
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-18 22:42:57 +0530

    Quiz

---
+++ Models/Quiz.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Learning.Models
+{
+	public class Quiz
+	{
+		public int QuizId { get; set; }
+		public string Title { get; set; }
+		public string Subject { get; set; }
+		public string Level { get; set; }  // Example: Easy, Medium, Hard
+		public List<QuizQuestion> Questions { get; set; } = new List<QuizQuestion>();
+		[NotMapped]
+		public int? CourseId { get; internal set; }
+	}
+
+}
