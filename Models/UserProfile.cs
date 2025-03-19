569f34256cf5744797a69145aa427d5dfe755252
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-19 08:45:14 +0530

    userfrofiles

---
+++ Models/UserProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Learning.Models
+{
+	public class UserProfile
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string ProfilePicture { get; set; }
+	}
+}
