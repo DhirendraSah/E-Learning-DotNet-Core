297689e0b76e59d544a1b512f0d7621f73e08a39
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2025-03-19 09:07:35 +0530

    token

---
+++ Models/PasswordResetToken.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Learning.Models
+{
+	public class PasswordResetToken
+	{
+		public int Id { get; set; }
+		public string Email { get; set; }
+		public string Token { get; set; }
+		public DateTime ExpiryDate { get; set; }
+	}
+
+}
